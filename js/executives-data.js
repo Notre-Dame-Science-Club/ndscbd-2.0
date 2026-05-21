4117674d6f374b846e51fc6101a93aa020320753
Samin Sakur <72861948+saminsakur@users.noreply.github.com>
2026-05-21 23:54:28 +0600

    Update executive positions in executives-data.js

--- js/executives-data.js
+++ js/executives-data.js
@@ -598,11 +598,11 @@ window.PANELS = [
       { name: "Mahadi Mohammad Bhuiyan",         position: "President", department: "LWS",      image:"pr(lws)-22.jpg",         social: {} },
       { name: "Hasibul Alam",         position: "President", department: "ICT",               social: { whatsapp: "https://wa.me/+8801552494638", linkedin: "https://www.linkedin.com/in/hasibul-alam-prionto-1083aa254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" } },
       { name: "Md. Jubayer Islam",    position: "Associate General Secretary",                social: {} },
-      { name: "Nayon Roy",            position: "Senior Vice President",                      social: {} },
+      { name: "Noor Alam Rohan",            position: "Senior Vice President",                      social: {} },
       { name: "Sadman Al Jami",       position: "Vice President", department: "Project",      image:"vp(project)-22.jpg",  social: {} },
       { name: "Fahmid Alam",          position: "Vice President", department: "Publication",  image:"vp(pub)-22.jpg",  social: {} },
       { name: "Sadman Shahriar",      position: "Vice President", department: "LWS",          image:"vp(lws)-22.jpg",  social: {} },
-      { name: "Araktim Khan",         position: "Vice President", department: "ICT",          image:"vp(ict)-22.jpg",  social: {} }
+      { name: "Sudad Hasanat Salim",         position: "Vice President", department: "ICT",          image:"vp(ict)-22.jpg",  social: {} }
     ]
   },
 
